4bdad8de73963449541be9d22d587adc09517ec1
Darioxavierl <darport0212@gmail.com>
2024-08-08 21:20:42 -0500

    First Styles

--- BioLink/BioLink/components/link_button.py
+++ BioLink/BioLink/components/link_button.py
@@ -1,8 +1,23 @@
 import reflex as rx
+import BioLink.styles.styles as styles
 
-def link_button(text: str, url: str) -> rx.Component:
+def link_button(title: str, body: str, url: str) -> rx.Component:
     return  rx.link(
-        rx.button(text),
+        rx.button(
+            rx.hstack(
+                rx.icon(
+                    tag="arrow_right",
+                    width=styles.Size.DEFAULT,
+                    height=styles.Size.LARGE
+                ),
+                rx.vstack(
+                    rx.text(title, style=styles.button_tittle_style),
+                    rx.text(body,style=styles.button_body_style)
+                )
+                
+            )
+        ),
         href=url,
-        is_external=True
+        is_external=True,
+        width="100%"
     )
